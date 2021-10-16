9e60408b795bd203182ece284601391818df8cc1
Luciano Santos <luciano_z7@hotmail.com>
2021-10-16 19:49:50 -0300

    Adding functions for combinations

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"sort"
 )
 
@@ -12,7 +13,30 @@ type Booking struct {
 	stars float64
 }
 
-func KnapSack(bookings []Booking, maxStars float64, metric func(i, j int) bool) (r []Booking) {
+func main() {
+
+	bookValues := []Booking{
+		{name: "Lolapaluza", value: 289.0, time: 7200., stars: 10.},
+		{name: "NotreDame", value: 190.0, time: 2880., stars: 3.},
+		{name: "Shaluna", value: 195.0, time: 4320., stars: 4.},
+		{name: "LasNoches", value: 300.0, time: 1440., stars: 2.},
+		{name: "Bienvenue", value: 130.0, time: 5760., stars: 1.},
+		{name: "Cielo", value: 279.0, time: 8640., stars: 1.},
+		{name: "Amigos", value: 350.0, time: 10080., stars: 5.},
+		{name: "Donatello", value: 110.0, time: 2160., stars: 1.},
+	}
+
+	f := func(i, j int) bool {
+		return bookValues[i].value/bookValues[i].stars > bookValues[j].value/bookValues[j].stars
+	}
+
+	KnapSack(bookValues, 10, f)
+	v, s := BestCombination(bookValues, 10)
+	fmt.Println(v)
+	fmt.Println(s)
+}
+
+func KnapSack(bookings []Booking, maxStars float64, metric func(i, j int) bool) (r []Booking, r2 []Booking) {
 	sort.Slice(bookings, metric)
 
 	s := 0.
@@ -23,29 +47,55 @@ func KnapSack(bookings []Booking, maxStars float64, metric func(i, j int) bool) 
 			s += i.stars
 		}
 	}
-	fmt.Println(s)
 	fmt.Println(r)
 	return
 }
 
-var cap1 = 20.
+func PossibleCombinations(bookings []Booking, ch chan []Booking) {
+	defer close(ch)
 
-func main() {
+	p := int(math.Pow(2., float64(len(bookings))))
 
-	testItems1 := []Booking{
-		{name: "Lolapaluza", value: 289.0, time: 7200., stars: 10.},
-		{name: "NotreDame", value: 190.0, time: 2880., stars: 3.},
-		{name: "Shaluna", value: 195.0, time: 4320., stars: 4.},
-		{name: "LasNoches", value: 300.0, time: 1440., stars: 2.},
-		{name: "Bienvenue", value: 130.0, time: 5760., stars: 1.},
-		{name: "Cielo", value: 279.0, time: 8640., stars: 1.},
-		{name: "Amigos", value: 350.0, time: 10080., stars: 1.},
-		{name: "Donatello", value: 110.0, time: 2160., stars: 1.},
+	for i := 0; i < p; i++ {
+		set := []Booking{}
+		for j := 0; j < len(bookings); j++ {
+			if (i>>uint(j))&1 == 1 {
+				set = append(set, bookings[j])
+			}
+		}
+		ch <- set
 	}
+}
 
-	f := func(i, j int) bool {
-		return testItems1[i].value/testItems1[i].stars > testItems1[j].value/testItems1[j].stars
+func getSackStars(set []Booking) (r float64) {
+	for _, i := range set {
+		r += i.stars
 	}
+	return
+}
 
-	KnapSack(testItems1, cap1, f)
+func getSackValue(set []Booking) (r float64) {
+	for _, i := range set {
+		r += i.value
+	}
+	return
+}
+
+func BestCombination(bookings []Booking, maxStars float64) (float64, []Booking) {
+	bestVal := 0.
+	bestSack := []Booking{}
+
+	ch := make(chan []Booking)
+	go PossibleCombinations(bookings, ch)
+
+	for sack := range ch {
+		if getSackStars(sack) <= maxStars {
+			v := getSackValue(sack)
+			if v > bestVal {
+				bestVal = v
+				bestSack = sack
+			}
+		}
+	}
+	return bestVal, bestSack
 }
